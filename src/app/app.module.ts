83453eef14b92b21f6992d274c6a6adffdc69ff3
pevargasg <pevargas@valdosta.edu>
2017-09-19 01:21:19 -0700

    home page done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,26 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { HttpModule } from '@angular/http';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-
+import { MainPage } from '../pages/main/main';
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    MainPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    MainPage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,58 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import { Component,Output,EventEmitter,Injectable } from '@angular/core';
+import { NavController, AlertController} from 'ionic-angular';
+import {MainPage} from '../main/main';
+import 'rxjs/add/operator/map';
+import { Http } from '@angular/http';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
+@Injectable()
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  @Output() cleanInput :  EventEmitter<string>;
+  mainPage = MainPage;
+  passValue : string = "";
+  tempPass : string = "";
+  constructor(public navCtrl: NavController, private alertCtrl: AlertController,private http: Http) {
+    this.cleanInput = new EventEmitter();
   }
 
+ //https://api.myjson.com/bins/1alzx1
+ //http://myjson.com
+  changePage( myPass: string){
+    this.getJSON();
+    if(myPass == this.tempPass) {
+      this.navCtrl.push(this.mainPage);
+      this.passValue = '';
+    } else {
+      const alert = this.alertCtrl.create({
+        title:'Wrong Password',
+        subTitle: 'Please enter the correct password',
+        message: "if you don't know the password please contact the admin of the app.",
+        buttons: [
+          {
+            text: 'Close',
+            handler: ()=> {
+              this.passValue = '';
+              
+            }
+          }
+        ]
+      });
+      alert.present();
+    }
+  }
+
+  getJSON(){
+    return this.http.get('https://api.myjson.com/bins/1alzx1').map((res) => res.json()).subscribe(data => {
+     console.log(data.password);
+     this.tempPass= data.password;   
+    }, (rej) => {console.error("Could not load local data",rej)});
+  }
 }
+    
+
+  
+
+
