409c31d3005466313985a9fcea386608e2c154f5
pevargasg <pevargas@valdosta.edu>
2017-09-20 22:06:01 -0700

    home page icons

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -12,30 +12,40 @@ import { Http } from '@angular/http';
 export class HomePage {
   @Output() cleanInput :  EventEmitter<string>;
   mainPage = MainPage;
+  homePage = HomePage;
   passValue : string = "";
+  userValue : string = "";
   tempPass : string = "";
+  tempUser : string = "";
+  tempUsers : JSON;
+  tempFlag: boolean = false;
   constructor(public navCtrl: NavController, private alertCtrl: AlertController,private http: Http) {
     this.cleanInput = new EventEmitter();
   }
 
- //https://api.myjson.com/bins/1alzx1
+ //https://api.myjson.com/bins/ https://jsonblob.com/850ee672-9d9a-11e7-aa97-09434374a2b8 https://quarkbackend.com/getfile/wearetamo/hydmech //https://www.jasonbase.com
  //http://myjson.com
-  changePage( myPass: string){
-    this.getJSON();
-    if(myPass == this.tempPass) {
+  changePage( myPass: string, username:string){
+    for(const i of Object.keys(this.tempUsers)){
+      if(myPass == this.tempUsers[i].password && username == this.tempUsers[i].username){
+        this.tempFlag = true;
+      }
+    }
+    if(this.tempFlag) {
       this.navCtrl.push(this.mainPage);
       this.passValue = '';
+      this.userValue = '';
+      this.tempFlag = false;
     } else {
       const alert = this.alertCtrl.create({
-        title:'Wrong Password',
-        subTitle: 'Please enter the correct password',
-        message: "if you don't know the password please contact the admin of the app.",
+        title:'Wrong Username or Password',
+        subTitle: 'Please enter the correct username or Password',
+        message: "if you don't know the Username or Password please contact the admin of the app.",
         buttons: [
           {
             text: 'Close',
             handler: ()=> {
-              this.passValue = '';
-              
+              this.passValue = '';              
             }
           }
         ]
@@ -45,10 +55,15 @@ export class HomePage {
   }
 
   getJSON(){
-    return this.http.get('https://api.myjson.com/bins/1alzx1').map((res) => res.json()).subscribe(data => {
-     console.log(data.password);
-     this.tempPass= data.password;   
+    return this.http.get('https://www.jasonbase.com/things/B8Jm.json').map((res) => res.json()).subscribe(data => {
+     console.log(data.users);
+     this.tempUsers= data.users;     
     }, (rej) => {console.error("Could not load local data",rej)});
+  }//https://www.jasonbase.com/things/B8Jm/edit
+
+  ionViewDidEnter(){
+    this.getJSON();
+    console.log("enter");
   }
 }
     
