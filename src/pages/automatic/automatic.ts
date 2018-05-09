8501445e24ab877d53d19d8bf9b52501f0ebc0a9
pevargasg <pevargas@valdosta.edu>
2018-05-08 21:30:52 -0400

    page change from automatic page

--- src/pages/automatic/automatic.ts
+++ src/pages/automatic/automatic.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, MenuController } from 'ionic-angular';
+import { BandsawService }  from '../../app/shared/bandsaw.service'
+import { HorizontalPage } from '../horizontal/horizontal';
+import { VerticalPage } from '../vertical/vertical';
 
 /**
  * Generated class for the AutomaticPage page.
@@ -14,8 +17,10 @@ import { IonicPage, NavController, NavParams, MenuController } from 'ionic-angul
   templateUrl: 'automatic.html',
 })
 export class AutomaticPage {
+  horizontalPage = HorizontalPage;
+  verticalPage = VerticalPage;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private menuCtrl: MenuController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private menuCtrl: MenuController, private bandsawService : BandsawService) {
   }
 
   ionViewDidLoad() {
@@ -24,6 +29,14 @@ export class AutomaticPage {
 
   onOpenMenu(){
     this.menuCtrl.open();    
+  }
+
+  openPage() {
+    if(this.bandsawService.getOrinentation() === 'horizontal') {
+         this.navCtrl.push(this.horizontalPage);
+    } else {
+        this.navCtrl.push(this.verticalPage);
+    }
   }
 
 }
