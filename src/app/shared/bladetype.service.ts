096a3682271ccfef3ed8be03d309476298201eea
pevargasg <pevargas@valdosta.edu>
2018-07-04 20:58:24 -0400

    bladetype setup

--- src/app/shared/bladetype.service.ts
+++ src/app/shared/bladetype.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 
 @Injectable()
-export class BladetypeService {
+export class BladeTypeService {
     bladeType: string;
 
     setBladeType(bladeType) {
