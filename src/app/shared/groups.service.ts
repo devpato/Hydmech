32a9e160fd3842d9bc2b8c25eb6c369c1aa413ef
pevargasg <pevargas@valdosta.edu>
2018-07-06 15:29:24 -0400

    get carbon link

--- src/app/shared/groups.service.ts
+++ src/app/shared/groups.service.ts
@@ -5,11 +5,15 @@ import 'rxjs/add/operator/map'
 @Injectable()
 export class GroupsService {
     bimetalUrl = 'https://hydmech-59ab0.firebaseio.com/BIMETAL.json'
-
+    carbonUrl = 'https://hydmech-59ab0.firebaseio.com/CARBON.json'
     constructor(private http: Http) { }
 
     getBimetalGroups() {
         return this.http.get(this.bimetalUrl);
+    }
+
+    getcarbonGroups() {
+        return this.http.get(this.carbonUrl);
     }
 }
 
