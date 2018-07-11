528abdf752356449856e389b0fe6257a1f13a9e7
pevargasg <pevargas@valdosta.edu>
2018-07-11 10:31:37 -0400

    variable set in balance page

--- src/pages/balaceblade/balaceblade.ts
+++ src/pages/balaceblade/balaceblade.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, MenuController } from 'ionic-angular';
-
+import {BladeTypeService} from '../../app/shared/bladetype.service';
 /**
  * Generated class for the BalacebladePage page.
  *
@@ -14,11 +14,21 @@ import { IonicPage, NavController, NavParams, MenuController } from 'ionic-angul
   templateUrl: 'balaceblade.html',
 })
 export class BalacebladePage {
+  cuttingSpeed = 0;
+  feedRate = 0;
+  cuttingRate = 0;
+  optimumTooth = 0;
+  cutTime = 0;
+  cyecleTime = 0;
+  toothJob = 0;
+  selectedItem : any;
+  constructor(public navCtrl: NavController, public navParams: NavParams, private menuCtrl: MenuController, private bladeTypeService : BladeTypeService) {}
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private menuCtrl: MenuController) {
+  ionViewDidLoad() {
+    this.selectedItem = this.bladeTypeService.getSelectedItem();
+    this.cuttingSpeed = this.selectedItem.D;
+    this.feedRate =   this.selectedItem.F;
   }
-
-  ionViewDidLoad() {}
 
   onOpenMenu(){
     this.menuCtrl.open();    
